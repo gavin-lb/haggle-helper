83f05810ba49cbefc7a2a809f60d61dc3b64a412
gavin-lb <94984768+gavin-lb@users.noreply.github.com>
2026-06-29 21:08:06 +0100

    refactor: simplify TrackedItem class by using Lombok's @Getter annotation

--- src/main/java/com/hagglehelper/TrackedItem.java
+++ src/main/java/com/hagglehelper/TrackedItem.java
@@ -1,36 +1,21 @@
 package com.hagglehelper;
 
-public class TrackedItem {
-    private final int itemId;
-    private final String name;
-    private final int cost;
-    private final int value;
+import lombok.Getter;
 
-    public TrackedItem(int itemId, String name, int cost, int value) 
-    {
-        this.itemId = itemId;
-        this.name = name;
-        this.cost = cost;
-        this.value = value;
-    }
+@Getter
+public class TrackedItem
+{
+	private final int itemId;
+	private final String name;
+	private final int cost;
+	private final int value;
 
-    public int getItemId() 
-    {
-        return itemId;
-    }
+	public TrackedItem(int itemId, String name, int cost, int value)
+	{
+		this.itemId = itemId;
+		this.name = name;
+		this.cost = cost;
+		this.value = value;
+	}
 
-    public String getName() 
-    {
-        return name;
-    }
-
-    public int getCost() 
-    {
-        return cost;
-    }
-    
-    public int getValue() 
-    {
-        return value;
-    }
 }
