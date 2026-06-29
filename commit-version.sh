51ebe54ffca3a5de40e7582c102432ad21080d42
gavin-lb <94984768+gavin-lb@users.noreply.github.com>
2026-06-29 17:08:29 +0100

    chore: update script to use pathspec

--- commit-version.sh
+++ commit-version.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-version=$(grep '^version=' gradle.properties | cut -d= -f2-)
-
-if ! git diff --cached --quiet; then
-    git stash push --staged
-    trap 'git stash pop >/dev/null 2>&1 || true' EXIT
+if git diff --quiet -- gradle.properties; then
+    exit 0
 fi
 
+version=$(grep '^version=' gradle.properties | cut -d= -f2-)
+
 git add gradle.properties
-git commit -m "chore: update version $version"
+git commit gradle.properties -m "chore: update version $version"
